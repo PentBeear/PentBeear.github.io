189b2bc566dfaf63e70f2735737b8a915c0f18b8
PentBeear <43016309+PentBeear@users.noreply.github.com>
2020-06-10 20:50:03 -0500

    Update test.js

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ class hypixelWrapper {
             "blocks": [{
                 "opcode": "jsonGetInfo",
                 "blockType": "reporter",
-                "text": "GetBazaarItem: key: [key] product name: [product]",
+                "text": "GetBazaarItem: key: [key] product name: [product] select value: [oper]",
                 "arguments": {
                     "key": {
                         "type": "string",
@@ -15,17 +15,43 @@ class hypixelWrapper {
                     "product": {
                         "type": "string",
                         "defaultValue": "INK_SACK:3"
-                    }
+                    },
+                    "oper": {
+                        "type": "string",
+                        "menu": "selectValue",
+                        "defaultValue": ""
+                    },
                 }
             }],
+            "menus": {
+                selectValue: this._formatMenu(['BuyVolume', 'SellVolume', 'BuyPrice', 'SellPrice', 'BuyOrders', 'SellOrders']),
+            }
         };
     }
-    jsonGetInfo ({key, product}) {
+    jsonGetInfo ({key, product, oper}) {
     console.log(key);
     console.log(product);
     fetch("https://api.hypixel.net/skyblock/bazaar/product?key=" + key + "&productId=" + product)
     .then(response => response.json())
     .then(data => console.log(data));
+    if (oper === 'BuyVolume') {
+        return data.product_info.quick_status.buyVolume
+    }
+    if (oper === 'SellVolume') {
+        return data.product_info.quick_status.sellVolume
+    }
+    if (oper === 'BuyPrice') {
+        return data.product_info.quick_status.buyPrice
+    }
+    if (oper === 'SellPrice') {
+        return data.product_info.quick_status.sellPrice
+    }
+    if (oper === 'BuyOrders') {
+        return data.product_info.quick_status.buyOrders
+    }
+    if (oper === 'SellOrders') {
+        return data.product_info.quick_status.sellOrders
+    }
     
     
         
